294d7dd02effce2e978a1077cdf2f147b9698091
William Novak <novakwilliamr@gmail.com>
2022-09-06 22:34:49 -0300

    divs css

--- cadastro.php
+++ cadastro.php
@@ -45,19 +45,19 @@
         <form action="cadastro.php" method="POST">
             <h2 class="legend">Cadastrar Usuário</h2>
 
-            <div class="columns-div">
+            <div class="columns-div flex-div">
                 <div class="column1">
-                    <input type="text" name="username" placeholder="Nome de usúario" class="inputUser" required>
+                    <input type="text" name="username" placeholder="Nome de usúario" class="inputs" required>
                 
-                    <input type="password" name="password" placeholder="Senha" class="inputUser" required>
+                    <input type="password" name="password" placeholder="Senha" class="inputs" required>
                     
-                    <input type="password" name="confirmPassword" placeholder="Confirmar senha" class="inputUser" required>
+                    <input type="password" name="confirmPassword" placeholder="Confirmar senha" class="inputs" required>
 
-                    <input type="text" name="cnpj" placeholder="CNPJ" class="inputUser" required>
+                    <input type="text" name="cnpj" placeholder="CNPJ" class="inputs" required>
 
-                    <input type="text" name="fantasyName" placeholder="Nome fantasia" class="inputUser" required>
+                    <input type="text" name="fantasyName" placeholder="Nome fantasia" class="inputs" required>
                 
-                    <input type="text" name="reason" placeholder="Razão social" class="inputUser" required>
+                    <input type="text" name="reason" placeholder="Razão social" class="inputs" required>
                 </div>
                 
                 <div class="column2">
@@ -69,48 +69,53 @@
                         <label  for="receptor">Receptor</label>
                     </div>
 
-                    <input type="email" name="email" placeholder="E-mail" class="inputUser" required>
+                    <input type="email" name="email" placeholder="E-mail" class="inputs" required>
             
-                    <input type="tel" name="tel" placeholder="Telefone" class="inputUser" required>
+                    <input type="tel" name="tel" placeholder="Telefone" class="inputs" required>
                     
-                    <input type="text" name="cep" placeholder="CEP" class="inputUser" required>
-                    
-                    <select name="state" class="inputUser" required>
-                        <option value="" disable selected hidden>Estado</option>
-                        <option value="AC">AC</option>
-                        <option value="AL">AL</option>
-                        <option value="AP">AP</option>
-                        <option value="AM">AM</option>
-                        <option value="BA">BA</option>
-                        <option value="CE">CE</option>
-                        <option value="DF">DF</option>
-                        <option value="ES">ES</option>
-                        <option value="GO">GO</option>
-                        <option value="MA">MA</option>
-                        <option value="MT">MT</option>
-                        <option value="MS">MS</option>
-                        <option value="MG">MG</option>
-                        <option value="PA">PA</option>
-                        <option value="PB">PB</option>
-                        <option value="PR">PR</option>
-                        <option value="PE">PE</option>
-                        <option value="PI">PI</option>
-                        <option value="RR">RR</option>
-                        <option value="RO">RO</option>
-                        <option value="RJ">RJ</option>
-                        <option value="RN">RN</option>
-                        <option value="RS">RS</option>
-                        <option value="SC">SC</option>
-                        <option value="SP">SP</option>
-                        <option value="SE">SE</option>
-                        <option value="TO">TO</option>
-                    </select>
+                    <div class="flex-div">
+                        <input type="text" name="cep" placeholder="CEP" class="inputs firstInput" required>
+                        
+                        <select name="state" class="inputs secondInput" required>
+                            <option value="" disable selected hidden>Estado</option>
+                            <option value="AC">AC</option>
+                            <option value="AL">AL</option>
+                            <option value="AP">AP</option>
+                            <option value="AM">AM</option>
+                            <option value="BA">BA</option>
+                            <option value="CE">CE</option>
+                            <option value="DF">DF</option>
+                            <option value="ES">ES</option>
+                            <option value="GO">GO</option>
+                            <option value="MA">MA</option>
+                            <option value="MT">MT</option>
+                            <option value="MS">MS</option>
+                            <option value="MG">MG</option>
+                            <option value="PA">PA</option>
+                            <option value="PB">PB</option>
+                            <option value="PR">PR</option>
+                            <option value="PE">PE</option>
+                            <option value="PI">PI</option>
+                            <option value="RR">RR</option>
+                            <option value="RO">RO</option>
+                            <option value="RJ">RJ</option>
+                            <option value="RN">RN</option>
+                            <option value="RS">RS</option>
+                            <option value="SC">SC</option>
+                            <option value="SP">SP</option>
+                            <option value="SE">SE</option>
+                            <option value="TO">TO</option>
+                        </select>
+                    </div>
 
-                    <input type="text" name="city" placeholder="Cidade" class="inputUser" required>
+                    <input type="text" name="city" placeholder="Cidade" class="inputs" required>
 
-                    <input type="text" name="address" placeholder="Endereço" class="inputUser" required>
+                    <div class="flex-div">
+                        <input type="text" name="address" placeholder="Endereço" class="inputs firstInput" required>
 
-                    <input type="text" name="num" placeholder="Número" class="inputUser" required>
+                        <input type="text" name="num" placeholder="Número" class="inputs secondInput" required>
+                    </div>
+
                 </div>
             </div>
 
