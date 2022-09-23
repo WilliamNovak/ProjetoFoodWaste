0bc4ace6eb49871f14558a03f3777f85cae6d249
William Novak <novakwilliamr@gmail.com>
2022-09-23 00:55:37 -0300

    criada pagina alimentos

---
+++ alimentos.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    require_once("./template.php");
+?>
+    <link rel="stylesheet" type="text/css" href="styles/style.css" >
+    <link rel="stylesheet" type="text/css" href="styles/alimentosStyle.css">
+    <title>Food Waste - Alimentos</title>
+</head>
+<body>
+
+<?php
+    require_once("./navbar.php");
+?>
