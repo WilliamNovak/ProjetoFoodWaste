7480084d29f7a97256c80c4ebfe2bf59287ec62e
Lazaro Engel Fernandes <20191inf0020@AD.SPG.IFSUL.EDU.BR>
2022-11-07 08:26:13 -0300

    07112022

--- index.php
+++ index.php
@@ -19,6 +19,49 @@
     require_once("./navbar.php"); 
 ?>
 
+<h2>FOOD WASTE</h2>
+
+
+<div class="div-objetivos">
+    <div class="objetivos">
+        <div class="propag">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, voluptatibus doloremque consequuntur eum, nostrum sunt nihil officia non 
+        </div>
+
+        <div class="propag">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sapiente, earum beatae labore veritatis hic molestias laboriosam aspernatur 
+        </div>
+
+        <div class="propag">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa similique maxime asperiores officia consectetur sequi at! Illum voluptatem nulla, voluptates, 
+        </div>
+    </div>
+</div>
+
+<h2>COMO FUNCIONA?</h2>
+
+
+<img src="imgs/teste.svg" class="fluxo">
+
+<div class="div-ceos">
+    <div class="ceos">
+        <div class="card">
+            Dionantan Jocemar de Souza de Lima
+        </div>
+        
+        <div class="card">
+            Lázaro Engel Fernandes
+        </div>
+
+        <div class="card">
+            William Renan Novak
+        </div>
+    </div>
+</div>
+
+
+
+
 <script src="js/script.js"></script>
 
 <?php
