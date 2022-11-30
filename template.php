fbff69ede3205c4e263c9a128529fad38cd4a897
William Novak <novakwilliamr@gmail.com>
2022-11-30 13:47:51 -0300

    links

--- template.php
+++ template.php
@@ -6,12 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--Links Bootstrap-->
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
 
-
-
-    <script src="https://kit.fontawesome.com/8f0b9fa9e9.js" crossorigin="anonymous"></script> 
+    <script src="js/font-awesome.js"></script> 
     
     <!-- Javascript -->
     <script src="js/script.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
