a9c1c0cef1cd91f4103d0c586e7b2b401ec381c3
William Novak <novakwilliamr@gmail.com>
2023-01-11 10:40:38 -0300

    navbar toggler corrigido

--- navbar.php
+++ navbar.php
@@ -6,7 +6,7 @@
             </a>
         </div>
         <div class="links-div">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
