4a52bc8bb73a417048eab329127b55ef550f822d
William Novak <novakwilliamr@gmail.com>
2023-01-13 01:27:22 -0300

    style tela alimentos

--- alimentos.php
+++ alimentos.php
@@ -31,22 +31,22 @@
     require_once("./navbar.php");
 ?>
 
-  <div class="container">
+  <div class="container text-start fs-6">
     <div class="table-wrapper">
       <div class="table-title">
         <div class="row">
           <div class="col-sm-6">
             <h2>Alimentos</h2>
           </div>
-          <div class="col-sm-6">
-            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#foodModal">
+          <div class="col-sm-6 text-end">
+            <button type="button" class="btn btn-success mt-1" data-bs-toggle="modal" data-bs-target="#foodModal">
               <i class="fa-solid fa-plus"></i> 
               Cadastrar Alimento
             </button>
           </div>
         </div>
       </div>
-      <table class="table table-striped table-hover">
+      <table class="table table-striped table-hover text-center align-middle">
         <thead>
           <tr>
             <th scope="col">Alimento</th>
@@ -59,32 +59,46 @@
         </thead>
 
         <tbody>
-          <tr>
-            <?php
-              while($data = mysqli_fetch_array($res)){
+          <?php
+            while($data = mysqli_fetch_array($res)){
 
-                $typeId = $data['idtipo'];
-                $sql_food_type = "SELECT descricao_alimento FROM tipo_alimento WHERE idtipo_alimento = '$typeId'";
-                $typeRes = $conexao->query($sql_food_type);
-                while($typeData = mysqli_fetch_array($typeRes)){
-                  $foodType = $typeData['descricao_alimento'];
-                }
-
-                echo "<td>".$data['descricao']."</td>";
-                echo "<td>".$foodType."</td>";
-                echo "<td>".$data['prazo_validade']."</td>";
-                echo "<td>".$data['quantidade']."</td>";
-                echo "<td>".$data['situacao']."</td>";
-                echo "<td>
-                        <button class='btn btn-outline-dark' value=".$data['idalimento'].">Editar</button>
-                        <button class='btn btn-outline-success' value=".$data['idalimento'].">Doar</button>
-                        <button class='btn btn-outline-danger' value=".$data['idalimento'].">Excluir</button>
-                      </td>";
+              $typeId = $data['idtipo'];
+              $sql_food_type = "SELECT descricao_alimento FROM tipo_alimento WHERE idtipo_alimento = '$typeId'";
+              $typeRes = $conexao->query($sql_food_type);
+              while($typeData = mysqli_fetch_array($typeRes)){
+                $foodType = $typeData['descricao_alimento'];
               }
-            ?>
-          </tr>
+
+              echo "<tr>";
+              echo "<td>".$data['descricao']."</td>";
+              echo "<td>".$foodType."</td>";
+              echo "<td>".$data['prazo_validade']."</td>";
+              echo "<td>".$data['quantidade']."</td>";
+              echo "<td>".$data['situacao']."</td>";
+              echo "<td>
+                      <button class='btn btn-outline-dark' value=".$data['idalimento'].">Editar</button>
+                      <button class='btn btn-outline-success' value=".$data['idalimento'].">Doar</button>
+                      <button class='btn btn-outline-danger' value=".$data['idalimento'].">Excluir</button>
+                    </td>";
+              echo "</tr>";
+            }
+          ?>
         </tbody>
       </table>
+      <div class="clearfix">
+        <div class="hint-text d-flex justify-content-between">
+          <p>Showing <b>5</b> out of <b>100</b> entries</p>
+          <ul class="pagination lh-1">
+            <li class="page-item disabled"><a href="#" class="page-link">Previous</a></li>
+            <li class="page-item"><a href="#" class="page-link">1</a></li>
+            <li class="page-item"><a href="#" class="page-link">2</a></li>
+            <li class="page-item active"><a href="#" class="page-link">3</a></li>
+            <li class="page-item"><a href="#" class="page-link">4</a></li>
+            <li class="page-item"><a href="#" class="page-link">5</a></li>
+            <li class="page-item"><a href="#" class="page-link">Next</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 
