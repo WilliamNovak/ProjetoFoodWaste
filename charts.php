391581ebe0f1c56964d7fe3cc78064c510976791
William Novak <novakwilliamr@gmail.com>
2023-01-30 11:52:20 -0300

    testes dashboard

---
+++ charts.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    </script>
+  </head>
+  <body>
+    <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+  </body>
+</html>
+
+<!-- Cards -->
+
+<!-- Instituição que mais doou no ano -->
+<!-- Instituição que mais recebeu no ano -->
+<!-- Tipo alimento mais doado no ano -->
+<!-- Total doacoes no ano -->
+
+<!-- Charts -->
+
+<!-- 3 Instituições que mais doaram p/ mes -->
+<!-- 3 Instituições que mais receberam p/ mes -->
+<!-- Tipo alimento mais doado p/ mes -->
+<!-- Total doacoes p/ mes -->
+
+<!-- Maximizar regiao -->
