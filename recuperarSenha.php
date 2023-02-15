bc5106d553e333c156ad4f98b368598b9b5c5deb
William Novak <novakwilliamr@gmail.com>
2023-02-14 21:02:16 -0300

    corrigido paths phpmailer

--- recuperarSenha.php
+++ recuperarSenha.php
@@ -3,9 +3,9 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'path/to/PHPMailer/src/Exception.php';
-    require 'path/to/PHPMailer/src/PHPMailer.php';
-    require 'path/to/PHPMailer/src/SMTP.php';
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
 
     // Verifica se o formulário foi enviado
     if (isset($_POST['email'])) {
